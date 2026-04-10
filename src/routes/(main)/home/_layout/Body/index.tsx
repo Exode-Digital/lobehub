0bf6aecac4a04a2d68b29b1976f35baec027f209
rdmclin2 <rdmclin2@gmail.com>
2026-04-10 11:23:13 +0800

    chore: add back createagents

--- src/routes/(main)/home/_layout/Body/index.tsx
+++ src/routes/(main)/home/_layout/Body/index.tsx
@@ -7,8 +7,6 @@ import Recents from '@/routes/(main)/home/features/Recents';
 
 import Agent from './Agent';
 import BottomMenu from './BottomMenu';
-import InboxEntry from './InboxEntry';
-import NewAgentButton from './NewAgentButton';
 
 export enum GroupKey {
   Agent = 'agent',
@@ -19,19 +17,10 @@ export enum GroupKey {
 const Body = memo(() => {
   return (
     <Flexbox flex={1} justify={'space-between'} paddingInline={4}>
-      <Flexbox gap={4} style={{ flex: 1, overflow: 'auto' }}>
-        <Flexbox gap={1}>
-          <InboxEntry />
-          <NewAgentButton />
-        </Flexbox>
-        <Accordion
-          defaultExpandedKeys={[GroupKey.Recents, GroupKey.Project, GroupKey.Agent]}
-          gap={8}
-        >
-          <Recents itemKey={GroupKey.Recents} />
-          <Agent itemKey={GroupKey.Agent} />
-        </Accordion>
-      </Flexbox>
+      <Accordion defaultExpandedKeys={[GroupKey.Recents, GroupKey.Project, GroupKey.Agent]} gap={8}>
+        <Recents itemKey={GroupKey.Recents} />
+        <Agent itemKey={GroupKey.Agent} />
+      </Accordion>
       <BottomMenu />
     </Flexbox>
   );
--- src/routes/(main)/home/_layout/Header/index.tsx
+++ src/routes/(main)/home/_layout/Header/index.tsx
@@ -4,6 +4,7 @@ import { memo } from 'react';
 
 import SideBarHeaderLayout from '@/features/NavPanel/SideBarHeaderLayout';
 
+import AddButton from './components/AddButton';
 import InboxButton from './components/InboxButton';
 import Nav from './components/Nav';
 import User from './components/User';
@@ -17,6 +18,7 @@ const Header = memo(() => {
         right={
           <>
             <InboxButton />
+            <AddButton />
           </>
         }
       />
